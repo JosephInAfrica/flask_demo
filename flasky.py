cd7047404851f038c30b79a0340dc8680b42c6e3
JosephInAfrica <lochleee@gmail.com>
2018-06-06 02:15:05 +0800

    to deploy demo

--- flasky.py
+++ flasky.py
@@ -1,19 +1,12 @@
-import os
-from app import create_app, db
-from app.models import User, Role
-from flask_migrate import Migrate
+from flask import Flask
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-migrate = Migrate(app, db)
+app = Flask(__name__)
 
 
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return 'hello world'
 
 
-@app.cli.command()
-def test():
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+if __name__ == '__main__':
+    app.run()
