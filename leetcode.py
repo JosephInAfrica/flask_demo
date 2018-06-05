9ccf8310d58986fec9e8893dc5abe259723117f4
JosephInAfrica <lochleee@gmail.com>
2018-06-06 03:28:56 +0800

    new land

---
+++ leetcode.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    nums = [11, 7, 2, 15]
+    target = 9
+
+    def twoSum(self, nums, target):
+        length = len(nums)
+        for i in range(length):
+            for j in range(i + 1, length):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+s = Solution()
+print(s.twoSum(s.nums, s.target))
